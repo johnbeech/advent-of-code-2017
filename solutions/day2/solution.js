b8296e5982c2647ed9e1a3852d815750c1eaecab
John Beech <john.beech@bbc.co.uk>
2017-12-05 09:55:10 +0000

    Solve day2 part2

--- solutions/day2/solution.js
+++ solutions/day2/solution.js
@@ -7,26 +7,55 @@ function sum (arr) {
   return arr.reduce((acc, d) => acc + d, 0)
 }
 
-function calculateCheckSum(row) {
+function solution1CheckSum (row) {
   const min = Math.min(...row)
   const max = Math.max(...row)
   return max - min
+}
+
+function findDivisor (cell, arr) {
+  arr = [].concat(arr)
+  while (arr.length > 0) {
+    let next = arr.pop()
+    if (next !== cell && cell / next === Math.round(cell / next)) {
+      return next
+    }
+  }
+  return 0
+}
+
+function solution2CheckSum (row) {
+  return row.map(cell => {
+    let divisor = findDivisor(cell, row)
+    if (divisor) {
+      return cell / divisor
+    }
+  }).filter(i => i)[0]
 }
 
 async function run () {
   const input = await read(path.join(__dirname, 'input.txt'), 'utf8')
 
   const rows = input.trim().split(NL).map(row => row.split(TAB).map(cell => Number.parseInt(cell)))
-  const rowCheckSums = rows.map(calculateCheckSum)
+  const solution1CheckSums = rows.map(solution1CheckSum)
 
-  let solution = sum(rowCheckSums)
+  console.log('Solution 1 Checksums', solution1CheckSums.join(' : '))
 
-  report(input, solution)
+  let solution1 = sum(solution1CheckSums)
+
+  const solution2CheckSums = rows.map(solution2CheckSum)
+
+  console.log('Solution 2 Checksums', solution2CheckSums.join(' : '))
+
+  let solution2 = sum(solution2CheckSums)
+
+  report(input, solution1, solution2)
 }
 
-function report (input, solution) {
+function report (input, solution1, solution2) {
   const solutionName = __dirname.split(path.sep).pop()
-  console.log('Advent of Code 2017 :', solutionName, 'solution for', NL, input, ':', solution)
+  console.log('Advent of Code 2017 :', solutionName, 'solution 1', ':', solution1)
+  console.log('Advent of Code 2017 :', solutionName, 'solution 2', ':', solution2)
 }
 
 run()
