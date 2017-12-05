176856bb22e60dd68cbb06195796d236255b26d4
John Beech <john.beech@bbc.co.uk>
2017-12-05 09:04:47 +0000

    Force linting

--- solutions/template/solution.js
+++ solutions/template/solution.js
@@ -1,14 +1,14 @@
 const path = require('path')
 const {read} = require('promise-path')
 
-async function run() {
+async function run () {
   const input = await read(path.join(__dirname, 'input.txt'), 'utf8')
   let solution = 'UNSOLVED'
 
   report(input, solution)
 }
 
-function report(input, solution) {
+function report (input, solution) {
   const solutionName = __dirname.split(path.sep).pop()
   console.log('Advent of Code 2017 :', solutionName, 'solution for', input, ':', solution)
 }
