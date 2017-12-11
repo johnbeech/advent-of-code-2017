b04819f58e453d68672e2f085fc5e9545f1b56f2
John Beech <john.beech@bbc.co.uk>
2017-12-11 08:39:54 +0000

    Solve day 8

--- solutions/day8/solution.js
+++ solutions/day8/solution.js
@@ -1,8 +1,59 @@
 const path = require('path')
 const {read} = require('promise-path')
+const NL = '\n'
+
+const instructionMatcher = /([A-z]+) (inc|dec) (-?\d+) if ([A-z]+) (>|<|<=|>=|==|!=) (-?\d+)/i // b inc 5 if a > 1
+function parseInstruction (line) {
+  const part = line.match(instructionMatcher) || []
+  if (part.length === 0) {
+    console.log('Unable to match', line)
+    return false
+  }
+
+  return {
+    operationRegister: part[1],
+    operation: part[2],
+    operationValue: parseInt(part[3]),
+    conditionRegister: part[4],
+    condition: part[5],
+    conditionValue: parseInt(part[6])
+  }
+}
+
+const conditionals = {
+  '>': (a, b) => a > b,
+  '<': (a, b) => a < b,
+  '<=': (a, b) => a <= b,
+  '>=': (a, b) => a >= b,
+  '==': (a, b) => a === b,
+  '!=': (a, b) => a !== b
+}
+
+const operations = {
+  'inc': (a, b) => a + b,
+  'dec': (a, b) => a - b
+}
+
+function processInstruction (instruction, register) {
+  register[instruction.operationRegister] = register[instruction.operationRegister] || 0
+  register[instruction.conditionRegister] = register[instruction.conditionRegister] || 0
+
+  const check = conditionals[instruction.condition]
+  if (check(register[instruction.conditionRegister], instruction.conditionValue)) {
+    const operation = operations[instruction.operation]
+    register[instruction.operationRegister] = operation(register[instruction.operationRegister], instruction.operationValue)
+  }
+}
 
 function solve (input) {
-  return 'UNSOLVED'
+  const instructions = input.split(NL).map(n => n.trim()).filter(n => n).map(parseInstruction)
+
+  const register = {}
+  instructions.forEach(instruction => processInstruction(instruction, register))
+
+  const maxRegister = Math.max(...Object.keys(register).map(key => register[key]))
+
+  return maxRegister
 }
 
 async function run () {
