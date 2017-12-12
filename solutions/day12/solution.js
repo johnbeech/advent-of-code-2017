f0a10966625f3b4247b89948940f65747ae9559f
John Beech <john.beech@bbc.co.uk>
2017-12-12 20:01:38 +0000

    Solve day 12

--- solutions/day12/solution.js
+++ solutions/day12/solution.js
@@ -1,10 +1,44 @@
 const path = require('path')
 const {read} = require('promise-path')
+const NL = '\n'
+
+const connectionMatcher = /(\d+) <-> ([\d, ]+)/ // 4 <-> 2, 3, 6
+function parseLine (line) {
+  const matches = line.match(connectionMatcher)
+  return {
+    id: parseInt(matches[1]),
+    connections: matches[2].split(', ').map(n => parseInt(n))
+  }
+}
+
+function scan (node, group = {}) {
+  if (group[node.id]) {
+    return false
+  }
+  group[node.id] = true
+  node.connections.forEach(n => scan(n, group))
+
+  return Object.keys(group)
+}
 
 function solve (input) {
+  const nodes = input.split(NL).map(n => n.trim()).filter(n => n).map(parseLine)
+
+  const index = {}
+
+  nodes.forEach(node => {
+    index[node.id] = node
+  })
+
+  nodes.forEach(node => {
+    node.connections = node.connections.map(n => index[n])
+  })
+
+  const group = scan(index[0])
+
   return {
     input,
-    actual: 'UNSOLVED'
+    actual: group.length + ' : ' + group.join(', ')
   }
 }
 
