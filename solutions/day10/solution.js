027fca72c94d49ba593d6dc12690afa9f4cf20dd
John Beech <john.beech@bbc.co.uk>
2017-12-12 11:55:52 +0000

    Solve day 10 part 2

--- solutions/day10/solution.js
+++ solutions/day10/solution.js
@@ -5,11 +5,11 @@ const NL = '\n'
 function parseTest (line) {
   const parts = line.split(':')
   return {
-    input: line,
     length: parseInt(parts[0]),
-    lengths: parts[1].split(',').map(n => parseInt(n)),
+    input: parts[1],
     actual: 0,
-    expected: parts[2]
+    expected: parts[2],
+    expectedHash: parts[3]
   }
 }
 
@@ -33,10 +33,11 @@ function setCircularSection (array, start, data) {
   }
 }
 
-function solve (test) {
+function solve1 (test) {
   test.data = []
   test.skipSize = 0
   test.position = 0
+  test.lengths = test.input.split(',').map(n => parseInt(n))
   while (test.data.length < test.length) {
     test.data.push(test.data.length)
   }
@@ -55,18 +56,72 @@ function solve (test) {
   return test
 }
 
+function zp (str) {
+  if (str.length < 2) {
+    return '0' + str
+  }
+  return str
+}
+
+function hexCode (value) {
+  return zp(value.toString(16))
+}
+
+function solve2 (test) {
+  test.data = []
+  test.skipSize = 0
+  test.position = 0
+  test.lengths = test.input.split('').map(n => (n + '').charCodeAt(0)).concat(17, 31, 73, 47, 23)
+  test.expected = test.expectedHash
+  while (test.data.length < test.length) {
+    test.data.push(test.data.length)
+  }
+
+  for (let r = 0; r < 64; r++) {
+    test.lengths.forEach(length => {
+      const section = readCircularSection(test.data, test.position, length)
+      section.reverse()
+      setCircularSection(test.data, test.position, section)
+
+      test.position = test.position + test.skipSize + length
+      test.skipSize++
+    })
+  }
+
+  test.sparseHash = [].concat(test.data)
+  test.denseHash = bitwiseBlock(test.sparseHash, 16)
+  test.knotHash = test.denseHash.map(hexCode).join('')
+
+  test.actual = test.knotHash
+
+  return test
+}
+
+function bitwiseBlock (array, blockSize) {
+  const result = []
+  for (let i = 0; i < blockSize; i++) {
+    result[i] = 0
+    for (let j = 0; j < blockSize; j++) {
+      result[i] = result[i] ^ array[(i * blockSize) + j]
+    }
+  }
+  return result
+}
+
 async function run () {
-  const input = await read(path.join(__dirname, 'input.txt'), 'utf8')
-  const tests = parseTests(input)
+  const input1 = await read(path.join(__dirname, 'input.txt'), 'utf8')
+  const input2 = await read(path.join(__dirname, 'input-part2.txt'), 'utf8')
 
-  const solutions = tests.map(solve)
+  const solutions1 = parseTests(input1).map(solve1)
+  const solutions2 = parseTests(input2).map(solve2)
 
-  solutions.map(report)
+  solutions1.map(report)
+  solutions2.map(report)
 }
 
 function report (test) {
   const solutionName = __dirname.split(path.sep).pop()
-  console.log('Advent of Code 2017 :', solutionName, 'solution for', test.input, ':', test.actual, `(${test.expected})`)
+  console.log('Advent of Code 2017 :', solutionName, 'solution for', `"${test.input}"`, ':', test.actual, `(${test.expected})`)
 }
 
 run()
