1b4a6e6c747c56dbec883cee3f19e8dc882b4164
John Beech <john.beech@bbc.co.uk>
2017-12-14 10:00:24 +0000

    Stub solution for day14

---
+++ solutions/day14/solution.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const {read} = require('promise-path')
+const NL = '\n'
+
+function solve (key) {
+  return {
+    key
+  }
+}
+
+function print8x8 (solution) {
+  const size = 8
+  let row
+  let key
+
+  console.log('Solution for:', solution.key)
+  for (let j = 0; j < size; j++) {
+    row = []
+    for (let i = 0; i < size; i++) {
+      key = `${i},${j}`
+      row.push(solution[key] ? '#' : '.')
+    }
+    console.log(row.join(' '))
+  }
+  console.log('')
+}
+
+async function run () {
+  const input = await read(path.join(__dirname, 'input.txt'), 'utf8')
+  let solution = 'UNSOLVED'
+
+  const keys = input.split(NL).map(n => n.trim()).filter(n => n)
+
+  const solutions = keys.map(solve)
+
+  solutions.map(print8x8)
+
+  report(input, solution)
+}
+
+function report (input, solution) {
+  const solutionName = __dirname.split(path.sep).pop()
+  console.log('Advent of Code 2017 :', solutionName, 'solution for', input, ':', solution)
+}
+
+run()
