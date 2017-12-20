c2ea57b77d861261a5c5b37a1c56bd38d498df37
John Beech <john.beech@bbc.co.uk>
2017-12-20 05:12:24 +0000

    Stub parsing for day 20

---
+++ solutions/day20/solution.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const {read} = require('promise-path')
+const NL = '\n'
+
+function parseVector (vector) {
+  return vector.substr(3).replace('>', '').split(',').map(n => parseInt(n))
+}
+
+function parseLine (line) {
+  const parts = line.split(', ').map(p => p.trim())
+  return {
+    p: parseVector(parts[0]),
+    v: parseVector(parts[1]),
+    a: parseVector(parts[2])
+  }
+}
+
+function solve (input) {
+  console.log('Solve', input)
+
+  return {
+    input: input.length + ' particles',
+    actual: 'unknown'
+  }
+}
+
+function parseInput (input) {
+  return input.trim().split(NL).map(parseLine)
+}
+
+async function run () {
+  const sampleinput = await read(path.join(__dirname, 'sampleinput.txt'), 'utf8')
+  const input = await read(path.join(__dirname, 'input.txt'), 'utf8')
+
+  const solutions = [sampleinput, input].map(parseInput).map(solve)
+
+  solutions.forEach(report)
+}
+
+/*
+p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>    -4 -3 -2 -1  0  1  2  3  4
+p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>
+*/
+
+function report (solution) {
+  const solutionName = __dirname.split(path.sep).pop()
+  console.log('Advent of Code 2017 :', solutionName, 'solution for', solution.input, ':', solution.actual)
+}
+
+run()
